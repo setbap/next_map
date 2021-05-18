e4aacf16afdc87332dda66b69a01c531b6d4fc9a
setbaq <ebr.sina@outlook.com>
2021-05-18 22:45:58 +0430

    highlite selected cities

--- src/components/landfill/CitiesButton.tsx
+++ src/components/landfill/CitiesButton.tsx
@@ -1,30 +1,70 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { pagesLinks } from "~/utils/links";
 
 const CitiesButtons = () => {
+  const router = useRouter().route;
   return (
     <div className=" w-full mx-auto text-center rounded-xl flex flex-wrap items-center justify-center">
-      <CityButton cityName="sari" name="ساری" />
-      <CityButton cityName="babol" name="بابل" />
-      <CityButton cityName="amol" name="آمل" />
-      <CityButton cityName="qaemshahr" name="قائمشهر" />
-      <CityButton cityName="freydoonkenar" name="فریدونکنار" />
-      <CityButton cityName="babolsar" name="بابلسر" />
-      <CityButton cityName="noshahr" name="نوشهر" />
+      <CityButton
+        isActive={router === pagesLinks.landfillsItem({ city: "sari" })}
+        cityName="sari"
+        name="ساری"
+      />
+      <CityButton
+        isActive={router === pagesLinks.landfillsItem({ city: "babol" })}
+        cityName="babol"
+        name="بابل"
+      />
+      <CityButton
+        isActive={router === pagesLinks.landfillsItem({ city: "amol" })}
+        cityName="amol"
+        name="آمل"
+      />
+      <CityButton
+        isActive={router === pagesLinks.landfillsItem({ city: "qaemshahr" })}
+        cityName="qaemshahr"
+        name="قائمشهر"
+      />
+      <CityButton
+        isActive={
+          router === pagesLinks.landfillsItem({ city: "freydoonkenar" })
+        }
+        cityName="freydoonkenar"
+        name="فریدونکنار"
+      />
+      <CityButton
+        isActive={router === pagesLinks.landfillsItem({ city: "babolsar" })}
+        cityName="babolsar"
+        name="بابلسر"
+      />
+      <CityButton
+        isActive={router === pagesLinks.landfillsItem({ city: "noshahr" })}
+        cityName="noshahr"
+        name="نوشهر"
+      />
     </div>
   );
 };
 
-const CityButton = ({ name, cityName }: { name: string; cityName: string }) => {
+const CityButton = ({
+  name,
+  cityName,
+  isActive,
+}: {
+  name: string;
+  cityName: string;
+  isActive: boolean;
+}) => {
   return (
     <Link scroll={true} href={pagesLinks.landfillsItem({ city: cityName })}>
       <a className="m-2">
         <button type="button" className="shadow-lg">
           <div>
             <div
-              className=" w-16 h-12 bg-green-100 
-              text-skin-primary flex flex-grow items-center justify-center
-              shadow-lg  rounded-md"
+              className={` w-16 h-12 ${isActive ? "bg-white" : "bg-green-100"}
+              text-skin-primary flex flex-grow items-center hover:shadow-xl justify-center
+              shadow-md  rounded-md`}
             >
               {name}
             </div>
